7a421691bb477d6eb7178e8d5a40b795950ed883
MohianEvan <h3is3nb3rg20@yahoo.com>
2020-08-11 02:29:14 +0600

    - Merry Christmaaas!

---
+++ 2483.cs
@@ -0,0 +1,17 @@
+using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+       int x = int.Parse(Console.ReadLine());
+            Console.Write("Feliz nat");
+            for (int i = 0; i < x; i++)
+            {
+                Console.Write("a");
+            }
+            Console.Write("l!\n");
+
+    }
+
+}
