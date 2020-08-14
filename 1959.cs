f23a822f9fd44e0fa372636571ce1368c37550d1
MohianEvan <h3is3nb3rg20@yahoo.com>
2020-08-15 00:37:58 +0600

    - Regular Simple Polygons

---
+++ 1959.cs
@@ -0,0 +1,14 @@
+using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+        string[] x = Console.ReadLine().Split();
+            long a = int.Parse(x[0]);
+            long b = int.Parse(x[1]);
+             long z = a * b;
+            Console.WriteLine(z);
+    }
+
+}
