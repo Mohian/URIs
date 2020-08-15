ccb8450badf20bef90389ba41353a3abba49d6a7
MohianEvan <h3is3nb3rg20@yahoo.com>
2020-08-16 01:08:23 +0600

    - Brazil World Cup

---
+++ 1564.cs
@@ -0,0 +1,30 @@
+using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+     string str;
+            while((str=Console.ReadLine())!= null)
+            {
+                if (str == null)
+                {
+                    break;
+                }
+                else
+                { 
+                    int x = int.Parse(str);
+                    if (x == 0)
+                    {
+                    Console.WriteLine("vai ter copa!"); 
+                    }
+                    else
+                    {
+                        Console.WriteLine("vai ter duas!");
+                    }
+                }
+            }
+
+    }
+
+}
