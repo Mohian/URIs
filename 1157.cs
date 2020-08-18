88608a4cbd9d2e1df3bc9f62eb7685dd5967fd0d
MohianEvan <h3is3nb3rg20@yahoo.com>
2020-08-18 23:01:15 +0600

    - Divisors I

---
+++ 1157.cs
@@ -0,0 +1,20 @@
+using System; 
+
+class URI 
+{
+
+    static void Main(string[] args) 
+    { 
+
+      int x = int.Parse(Console.ReadLine());
+                {
+                for(int i=1;i<=x;i++)
+                    if(x%i==0)
+                    {
+                        Console.WriteLine(i);
+                    }
+
+                }
+    }
+
+}
