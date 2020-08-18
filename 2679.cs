eb44b72c7d080ea08965cbf55c26959abc7c0e38
MohianEvan <h3is3nb3rg20@yahoo.com>
2020-08-18 22:54:02 +0600

    - Even Successor

---
+++ 2679.cs
@@ -0,0 +1,21 @@
+using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+    int x = int.Parse(Console.ReadLine());
+            if (x % 2 == 0)
+            {
+                x += 2;
+                Console.WriteLine(x);
+            }
+            else
+            {
+                x++;
+                Console.WriteLine(x);
+            }
+
+    }
+
+}
