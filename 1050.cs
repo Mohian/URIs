c2993acd146a048148790b036b006f4975f2b786
MohianEvan <h3is3nb3rg20@yahoo.com>
2020-08-18 23:07:55 +0600

    DDD

---
+++ 1050.cs
@@ -0,0 +1,46 @@
+using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+        int x = int.Parse(Console.ReadLine());
+            if(x==61)
+            {
+                Console.WriteLine("Brasilia");
+            }
+            else if (x == 71)
+            {
+                Console.WriteLine("Salvador");
+            }
+            else if (x == 11)
+            {
+                Console.WriteLine("Sao Paulo");
+            }
+            else if (x == 21)
+            {
+                Console.WriteLine("Rio de Janeiro");
+            }
+            else if (x == 32)
+            {
+                Console.WriteLine("Juiz de Fora");
+            }
+            else if (x == 19)
+            {
+                Console.WriteLine("Campinas");
+            }
+            else if (x == 27)
+            {
+                Console.WriteLine("Vitoria");
+            }
+            else if (x == 31)
+            {
+                Console.WriteLine("Belo Horijonte");
+            }
+            else
+            {
+                Console.WriteLine("DDD nao cadastrado");
+            }
+    }
+
+}
