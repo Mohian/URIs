0b6c701944fca268fd603f9661bc97ed49a1843e
MohianEvan <h3is3nb3rg20@yahoo.com>
2020-08-18 23:09:47 +0600

    - Even Numbers

---
+++ 1059.cs
@@ -0,0 +1,17 @@
+using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+      int i;
+            for (i = 1; i <=100; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+    }
+
+}
