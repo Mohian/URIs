5b4553bc93486cb4fa163ece9ca6aab14ef92551
MohianEvan <h3is3nb3rg20@yahoo.com>
2020-08-18 23:09:03 +0600

    - Month

---
+++ 1052.cs
@@ -0,0 +1,63 @@
+using System; 
+
+class URI 
+{
+
+    static void Main(string[] args) 
+    { 
+
+       string s = Console.ReadLine();
+            int N = Int32.Parse(s);
+
+            if (N == 1)
+            {
+                Console.WriteLine("January");
+            }
+            else if (N == 2)
+            {
+                Console.WriteLine("February");
+            }
+            else if (N == 3)
+            {
+                Console.WriteLine("March");
+            }
+            else if (N == 4)
+            {
+                Console.WriteLine("April");
+            }
+            else if (N == 5)
+            {
+                Console.WriteLine("May");
+            }
+            else if (N == 6)
+            {
+                Console.WriteLine("June");
+            }
+            else if (N == 7)
+            {
+                Console.WriteLine("July");
+            }
+            else if (N == 8)
+            {
+                Console.WriteLine("August");
+            }
+            else if (N == 9)
+            {
+                Console.WriteLine("September");
+            }
+            else if (N == 10)
+            {
+                Console.WriteLine("October");
+            }
+            else if (N == 11)
+            {
+                Console.WriteLine("November");
+            }
+            else if (N == 12)
+            {
+                Console.WriteLine("December");
+            }
+            
+    }
+
+}
